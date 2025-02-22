32109b374dc751cf6146ab4b2be4dc0dcf41bb97
Suraj Pandey <surajpandey7493@gmail.com>
2025-02-22 21:34:51 +0530

    tailwindcss add

---
+++ frontend/src/app.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className="bg-red-400 text-white p-4">
+  <h1>Suraj Pandey</h1>
+</div>
+
+    </>
+  )
+}
